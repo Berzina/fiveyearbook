97d1f079a95d2d9d2e8974651ad54cecc7ec772b
Ekaterina Khotko <ekhotko@plgn.ru>
2016-11-24 16:53:00 +0500

    Added pagination for responses

--- Fiveyearbook/book/views.py
+++ Fiveyearbook/book/views.py
@@ -4,11 +4,29 @@ from .models import Response, Question
 from django.utils import timezone
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
 
 # Create your views here.
 class DetailView(generic.DetailView):
   model = Question
   template_name = 'book/question.html'
+
+  def get_context_data(self, **kwargs):
+    question = self.get_object()
+    context = super(DetailView, self).get_context_data(**kwargs)
+
+    response_list = question.response_set.all()
+    paginator = Paginator(response_list, 5)
+
+    page = self.request.GET.get('page',1)
+
+    responses = paginator.page(page)
+
+    context['responses'] =  responses
+    return context 
+
 
 def response(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
